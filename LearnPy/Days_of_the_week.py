c595a6dfc26847bb48fc5d8f4f26cc382cdc7f19
MubasherShahzad51 <57072127+MubasherShahzad51@users.noreply.github.com>
2019-10-31 19:10:26 -0700

    This is my fourth commit to show days of the week program

---
+++ LearnPy/Days_of_the_week.py
@@ -0,0 +1,5 @@
+# Program to find days of the week using format function
+days_of_the_week = "monday,tuesday,wednesday,thursday,friday,saturday,sunday"
+week_days = """days_of_the_week : {}"""
+week = days_of_the_week.format(days_of_the_week)
+print(week)
